bfbc07f070a233f51aaa0a8211577adb3b572d38
Roger D. Peng [amelia] <rdpeng@gmail.com>
2009-05-13 17:19:29 -0400

    Use packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,5 @@
     desc <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
     msg <- paste(desc[, "Title"], " (version ",
                  as.character(desc[, "Version"]), ")\n", sep = "")
-    writeLines(strwrap(msg))
+    packageStartupMessage(strwrap(msg))
 }
