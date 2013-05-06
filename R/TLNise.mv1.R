7eb117a3c621ba8feed8fe78e2f4c75314325f7d
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-05-06 10:37:42 -0400

    Add GPL-3 copyright notice

--- R/TLNise.mv1.R
+++ R/TLNise.mv1.R
@@ -5,6 +5,21 @@
 ######################################################################
 ## Minor changes for R port Copyright (C) 2004-2005, Roger D. Peng <rpeng@jhsph.edu>
 ######################################################################
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+###############################################################################
+
 
 tlnise<-function(Y,V,w=NA,V0=NA,prior=NA,N=1000,seed=NULL,
                  Tol=1e-6,maxiter=1000,intercept=TRUE,labelY=NA,labelYj=NA,
--- src/tlnisemv1.f
+++ src/tlnisemv1.f
@@ -1,3 +1,21 @@
+C############# Copyright 2000, Phil Everson, Swarthmore College. ##############
+C#####################################################################
+C# Minor changes for R port Copyright (C) 2004-2005, Roger D. Peng <rpeng@jhsph.edu>
+C#####################################################################
+C# This program is free software; you can redistribute it and/or modify
+C# it under the terms of the GNU General Public License as published by
+C# the Free Software Foundation; either version 2 of the License, or
+C# (at your option) any later version.
+C#
+C# This program is distributed in the hope that it will be useful,
+C# but WITHOUT ANY WARRANTY; without even the implied warranty of
+C# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C# GNU General Public License for more details.
+C#
+C# You should have received a copy of the GNU General Public License
+C# along with this program; if not, write to the Free Software
+C# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 C This file contains subroutines called by the Splus function "tlnise"
 C (version mv1). (See Everson & Morris (2000) JRSS-B for details).
 C  
